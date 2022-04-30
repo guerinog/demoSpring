a5cc8fa2e0083b9cccef7bc60b92e2594d7161a0
Graziano <graziano.pascale@gpi.it>
2022-04-30 19:27:50 +0200

    aggiunta dipendenza grazianofx

--- src/main/java/it/guerino/demoSpring/DemoSpringApplication.java
+++ src/main/java/it/guerino/demoSpring/DemoSpringApplication.java
@@ -1,7 +1,10 @@
 package it.guerino.demoSpring;
 
 import org.apache.logging.log4j.LogManager;
+//import it.graziano.grazianofx.*;
 import org.apache.logging.log4j.Logger;
+//import org.joda.time.format.DateTimeFormat;
+//import org.joda.time.format.DateTimeFormatter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,6 +17,11 @@ public class DemoSpringApplication {
 	public static void main(String[] args) {
 		loggerMain.debug("Inizio main");
 		SpringApplication.run(DemoSpringApplication.class, args);
-	}
+		//Validate validazione = new Validate();
+		//boolean validateByFindPatternCharSpecial = validazione.validateByFindPatternCharSpecial("dsfsdfsd5$£343");
+//DateTimeFormatter dtf = DateTimeFormat.forPattern("dd/MM/yyyy");
+//System.out.println("DATA: " + dtf.print(new Date().getTime()));
+}
 
+	
 }
